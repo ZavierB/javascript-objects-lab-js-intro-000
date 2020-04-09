80d8fcdfafda93a1c6c563fa33edc0c9dedee2f1
Zavier Berolo <zavier.berolo@gmail.com>
2020-04-09 08:40:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ function updateObjectWithKeyAndValue(object,key,value){
 var newObj = Object.assign({}, object)
 newObj[key] = value
   return newObj
+}
 
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key] = value
+  return object
 }
